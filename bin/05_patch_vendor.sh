c01c338de0c5b6d9c545621434f65aeaff0ee189
Antony K. S. <github@smankusors.com>
2025-07-20 02:56:31 +0700

    feat(bin): find & replace fstab instead of patching also * patch the vendor_file_contexts too * add log_from_init.rc for logging into sdcard
    Signed-off-by: Antony K. S. <github@smankusors.com>

--- bin/05_patch_vendor.sh
+++ bin/05_patch_vendor.sh
@@ -10,11 +10,23 @@ TMPDIR="$(mktemp -d)"
 echo "Mounting vendor to $TMPDIR..."
 mount work/vendor.img $TMPDIR
 
-echo "Patching..."
-cp patch/fstab.qcom $TMPDIR/etc/fstab.qcom
+if [[ "$1" == "--with-debug" ]]; then
+  echo "Debugging enabled, patching init.qcom.rc..."
+  cat patch/log_from_init.rc >> $TMPDIR/etc/init/hw/init.qcom.rc
+fi
+
+echo "Patching fstab.qcom..."
+sed -i '/c084000.sdhci/d' $TMPDIR/etc/fstab.qcom
+sed -i 's/\/dev\/block\/bootdevice\/by-name\/userdata/\/dev\/block\/platform\/soc\/c084000.sdhci\/by-name\/microsd_userdata/g' $TMPDIR/etc/fstab.qcom
+sed -i 's/\/dev\/block\/bootdevice\/by-name\/cache/\/dev\/block\/platform\/soc\/c084000.sdhci\/by-name\/microsd_cache/g' $TMPDIR/etc/fstab.qcom
+
+echo "mkdir $TMPDIR/internalcachesothatwecanbootfromsdcard..."
 mkdir $TMPDIR/internalcachesothatwecanbootfromsdcard | true
-echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_userdata u:object_r:userdata_block_device:s0" >> $TMPDIR/etc/selinux/vendor_file_contexts
+
+echo "Patching vendor_file_contexts..."
+echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_system u:object_r:system_block_device:s0" >> $TMPDIR/etc/selinux/vendor_file_contexts
 echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_cache u:object_r:cache_block_device:s0" >> $TMPDIR/etc/selinux/vendor_file_contexts
+echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_userdata u:object_r:userdata_block_device:s0" >> $TMPDIR/etc/selinux/vendor_file_contexts
 
 echo "Unmounting..."
 umount $TMPDIR
