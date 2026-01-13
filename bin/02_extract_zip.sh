998bcdc1a572c64249df28965167611726a0f066
Antony K. S. <github@smankusors.com>
2026-01-13 08:55:22 +0700

    feat(bin): conditional extract if it's not brottled
    Signed-off-by: Antony K. S. <github@smankusors.com>

--- bin/02_extract_zip.sh
+++ bin/02_extract_zip.sh
@@ -14,11 +14,20 @@ fi
 TARGET_ZIP="${zip_files[0]}"
 
 echo "Unzipping..."
-unzip -o "${TARGET_ZIP}" boot.img system.new.dat.br system.patch.dat system.transfer.list vendor.new.dat.br vendor.patch.dat vendor.transfer.list
+unzip -o "${TARGET_ZIP}" \
+  boot.img \
+  system.new.dat.br system.new.dat system.patch.dat system.transfer.list \
+  vendor.new.dat.br vendor.new.dat vendor.patch.dat vendor.transfer.list \
+  || true
 
-echo "Debrotling..."
-brotli --decompress --force system.new.dat.br
-brotli --decompress --force vendor.new.dat.br
+if [ -f system.new.dat.br ]; then
+  echo "Debrotling system.new.dat.br..."
+  brotli --decompress --force system.new.dat.br
+fi
+if [ -f vendor.new.dat.br ]; then
+  echo "Debrotling vendor.new.dat.br..."
+  brotli --decompress --force vendor.new.dat.br
+fi
 
 echo "Desparsing..."
 python3 /root/sdat2img/sdat2img.py system.transfer.list system.new.dat
