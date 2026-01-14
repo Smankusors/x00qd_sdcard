09e2132a8715d40e0b7bdbb4d15049e01e65f5ea
Antony K. S. <github@smankusors.com>
2026-01-15 00:50:55 +0700

    feat: config
    prepare for other qcom SoCs support
    
    also reorder the boot partition into first partition, since lk2nd
    only support reading first partition only for some reason.
    
    Signed-off-by: Antony K. S. <github@smankusors.com>

--- bin/05_patch_vendor.sh
+++ bin/05_patch_vendor.sh
@@ -6,6 +6,8 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+source work/config
+
 TMPDIR="$(mktemp -d)"
 echo "Mounting vendor to $TMPDIR..."
 mount work/vendor.img $TMPDIR
@@ -23,9 +25,9 @@ echo "Patching fstab.qcom..."
 FSTABQCOMFILE=$TMPDIR/etc/fstab.qcom
 cp $FSTABQCOMFILE $FSTABQCOMFILE.bak
 chcon --reference=$FSTABQCOMFILE $FSTABQCOMFILE.bak
-sed -i '/c084000.sdhci/d' $FSTABQCOMFILE
-sed -i 's/\/dev\/block\/bootdevice\/by-name\/userdata/\/dev\/block\/platform\/soc\/c084000.sdhci\/by-name\/microsd_userdata/g' $FSTABQCOMFILE
-sed -i 's/\/dev\/block\/bootdevice\/by-name\/cache/\/dev\/block\/platform\/soc\/c084000.sdhci\/by-name\/microsd_cache/g' $FSTABQCOMFILE
+sed -i "/$MICROSD_ADDR/d" $FSTABQCOMFILE
+sed -i "s/\/dev\/block\/bootdevice\/by-name\/userdata/\/dev\/block\/platform\/soc\/$MICROSD_ADDR\/by-name\/microsd_userdata/g" $FSTABQCOMFILE
+sed -i "s/\/dev\/block\/bootdevice\/by-name\/cache/\/dev\/block\/platform\/soc\/$MICROSD_ADDR\/by-name\/microsd_cache/g" $FSTABQCOMFILE
 chcon --reference=$FSTABQCOMFILE.bak $FSTABQCOMFILE
 rm $FSTABQCOMFILE.bak
 
@@ -38,9 +40,9 @@ cp $VENDORFILECONTEXTSFILE $VENDORFILECONTEXTSFILE.bak
 chcon --reference=$VENDORFILECONTEXTSFILE $VENDORFILECONTEXTSFILE.bak
 sed -i 's|^/dev/block/mmcblk1[[:space:]]\+u:object_r:sd_device:s0$|/dev/block/mmcblk1 u:object_r:root_block_device:s0|' $VENDORFILECONTEXTSFILE
 sed -i '/^\/dev\/block\/mmcblk1p1[[:space:]]/d' $VENDORFILECONTEXTSFILE
-echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_system u:object_r:system_block_device:s0" >> $VENDORFILECONTEXTSFILE
-echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_cache u:object_r:cache_block_device:s0" >> $VENDORFILECONTEXTSFILE
-echo "/dev/block/platform/soc/c084000.sdhci/by-name/microsd_userdata u:object_r:userdata_block_device:s0" >> $VENDORFILECONTEXTSFILE
+echo "/dev/block/platform/soc/$MICROSD_ADDR/by-name/microsd_system u:object_r:system_block_device:s0" >> $VENDORFILECONTEXTSFILE
+echo "/dev/block/platform/soc/$MICROSD_ADDR/by-name/microsd_cache u:object_r:cache_block_device:s0" >> $VENDORFILECONTEXTSFILE
+echo "/dev/block/platform/soc/$MICROSD_ADDR/by-name/microsd_userdata u:object_r:userdata_block_device:s0" >> $VENDORFILECONTEXTSFILE
 chcon --reference=$VENDORFILECONTEXTSFILE.bak $VENDORFILECONTEXTSFILE
 rm $VENDORFILECONTEXTSFILE.bak
 
